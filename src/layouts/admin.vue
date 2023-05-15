0cab34885f1a5f2d41b7b8a1cb43607dd45874e0
slince-zero <1711257704@qq.com>
2023-05-15 16:03:40 +0800

    keep-alive页面缓存---transition全局过渡动画

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -9,7 +9,13 @@
       </el-aside>
       <el-main>
         <f-tag-list></f-tag-list>
-        <router-view></router-view>
+        <router-view v-slot="{ Component }">
+          <transition name="fade">
+            <keep-alive :max="10">
+              <component :is="Component"></component>
+            </keep-alive>
+          </transition>
+        </router-view>
       </el-main>
     </el-container>
   </el-container>
@@ -24,5 +30,25 @@ import FTagList from "./components/FTagList.vue";
 <style>
 .el-aside {
   transition: all 0.2s;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.2;
+}
+.fade-enter-active {
+  transition-delay: 0.3s;
 }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>后台首页</div>
-  <div>{{ $store.state.user.username }}</div>
 </template>
 
 <script></script>
