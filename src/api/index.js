416266c7dd0cd9ca21f7cf38d38b8881fe43a4cc
slince-zero <1711257704@qq.com>
2023-05-18 08:25:14 +0800

    店铺和交易提示组件开发完成

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,14 @@
 // 后台首页接口
 import axios from "~/axios";
 
+// 请求首页那几个小卡片
 export function getStatistics1() {
   return axios.get("/admin/statistics1");
+}
+
+// 请求首页店铺和交易提示组件接口
+export function getStatistics2() {
+  return axios.get("/admin/statistics2");
 }
 
 // 请求首页echarts图表接口
